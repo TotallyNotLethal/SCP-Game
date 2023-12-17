0047b987723f12f6f4131684e47f8a25d47b08a2
Lethal <47657372+TotallyNotLethal@users.noreply.github.com>
2023-12-16 21:18:21 -0500

    Update SCP0049.cs

--- SCP/SCPs/SCP0049.cs
+++ SCP/SCPs/SCP0049.cs
@@ -1,18 +1,61 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class SCP0049 : MonoBehaviour
+public class SCP0049 : SCP
 {
-    // Start is called before the first frame update
-    void Start()
+    public float cureRange = 5.0f;
+    public LayerMask cureTargetsLayer;
+    public GameObject SCP0049_2Prefab;
+    private bool isCuring = false;
+
+    protected override void Start()
     {
-        
+        base.Start();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-        
+        base.Update();
+        PatrolAndWander();
+        CheckAndCureTargets();
+    }
+
+    private void PatrolAndWander()
+    {
+        if (!navAgent.hasPath)
+        {
+            Vector3 randomDirection = Random.insideUnitSphere * 10.0f;
+            randomDirection += transform.position;
+            NavMeshHit hit;
+            NavMesh.SamplePosition(randomDirection, out hit, 10.0f, NavMesh.AllAreas);
+            navAgent.SetDestination(hit.position);
+        }
+    }
+
+    private void CheckAndCureTargets()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, cureRange, cureTargetsLayer);
+        foreach (var hitCollider in hitColliders)
+        {
+            if (!isCuring && !hitCollider.GetComponent<SCP0049_2>())
+            {
+                StartCoroutine(CureTarget(hitCollider.gameObject));
+            }
+        }
+    }
+
+    private IEnumerator CureTarget(GameObject target)
+    {
+        isCuring = true;
+        navAgent.ResetPath();
+        transform.LookAt(target.transform);
+
+        yield return new WaitForSeconds(3);
+
+        Instantiate(SCP0049_2Prefab, target.transform.position, Quaternion.identity);
+        Destroy(target);
+
+        isCuring = false;
     }
 }
