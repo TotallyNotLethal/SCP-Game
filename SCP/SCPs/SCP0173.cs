10263a2251673f272604d176241c5e393c657936
Lethal <47657372+TotallyNotLethal@users.noreply.github.com>
2023-12-16 21:07:23 -0500

    Update SCP0173.cs

--- SCP/SCPs/SCP0173.cs
+++ SCP/SCPs/SCP0173.cs
@@ -8,9 +8,7 @@ public class SCP0173 : SCP
     public float MovementSpeed { get; private set; }
     public Vector3 LastKnownPlayerPosition { get; private set; }
     private float attackCooldown;
-    private const float AttackRange = 1f; // Example range for attack
-    //public GameObject spiritPrefab;
-    //private GameObject spiritInstance;
+    private const float AttackRange = 1f;
     public GameObject spiritBoundarySphere;
     public float spiritMoveSpeed = 8f;
     public float spiritRange = 8f;
@@ -24,7 +22,7 @@ public class SCP0173 : SCP
         base.Awake();
         InitializeSCP("SCP-0173", "The Sculpture", SCPClass.Euclid, "A humanoid sculpture that moves rapidly when unobserved.");
         IsAggressive = true;
-        MovementSpeed = 10f; // Example speed when unobserved
+        MovementSpeed = 10f;
         attackCooldown = 0f;
     }
     protected override void Start()
@@ -43,7 +41,6 @@ public class SCP0173 : SCP
     {
         if (!isBeingObserved)
         {
-            //MoveTowardsTarget(LastKnownPlayerPosition);
             if (IsPlayerInRange(player))
             {
                 if (attackCooldown <= 0)
@@ -80,14 +77,11 @@ public class SCP0173 : SCP
         float angleToPlayer = Vector3.Angle(player.transform.forward, directionToSCP);
         float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
 
-        // Check if SCP-173 is within the player's field of view and within a certain distance
-        if (angleToPlayer <= 45.0f && distanceToPlayer <= 20.0f) // Example values for FOV angle and max distance
+        if (angleToPlayer <= 45.0f && distanceToPlayer <= 20.0f)
         {
             RaycastHit hit;
-            // Raycast to check if there is a direct line of sight
             if (Physics.Raycast(player.transform.position, directionToSCP, out hit, distanceToPlayer))
             {
-                // Check if the raycast hits SCP-173 and not another object
                 return hit.transform == transform;
             }
         }
@@ -107,17 +101,15 @@ public class SCP0173 : SCP
     {
         if (isBeingObserved)
         {
-            // Freeze SCP-173 and activate spirit mode
             navAgent.isStopped = true;
             CaughtByObserver();
             MoveSpirit();
         }
         else
         {
-            // Allow SCP-173 to move and deactivate spirit mode
             navAgent.isStopped = false;
             FreeFromObserver();
-            //ActivateEffect(); // Continue with SCP-173's usual behavior
+            ActivateEffect();
         }
     }
 
@@ -138,67 +130,46 @@ public class SCP0173 : SCP
     private void Attack(Player player)
     {
         player.Stats.SetHealth(0);
-        // Additionally, trigger any relevant effects, animations, or sounds
     }
-
-    // Additional SCP-173 specific methods
 
     public void InstantaneousMovement(Vector3 targetPosition)
     {
-        // Method to simulate instantaneous movement when not observed
         if (!isBeingObserved)
         {
             transform.position = targetPosition;
-            // Trigger any relevant effects (like sound, particle effects)
         }
     }
 
     public void CheckEnvironmentInteraction()
     {
-        // Check for interactions with the environment
-        // For example, SCP-173 could interact with specific objects or obstacles in its path
     }
 
     public void TriggerSpecialEffect()
     {
-        // Trigger a special effect unique to SCP-173
-        // This could be a visual effect, sound, or a gameplay mechanic
     }
 
     public void EmitSound()
     {
-        // SCP-173 can emit a distinct sound, contributing to the eerie atmosphere
-        // Play sound effect here
     }
 
     public void ApplyPsychologicalEffect(PlayerStats playerStats)
     {
-        // SCP-173 can have a psychological impact on the player
         playerStats.UpdateMentalHealth(-MentalHealthImpact);
-        // Implement additional psychological effect logic
     }
 
     public void AvoidLightSources()
     {
-        // SCP-173 may behave differently in light or darkness
-        // Implement logic for SCP-173 to avoid or react to light sources
     }
 
-    // Overriding base methods for SCP-173 specific behaviors
     public override void EncounterBehavior()
     {
-        // Define specific behavior when the player encounters SCP-173
         EmitSound();
-        //ApplyPsychologicalEffect(Stats);
-        // Additional encounter logic...
     }
 
     private void MoveSpirit()
     {
-        // Assume playerCamera is the camera attached to the player
         if (isBeingObserved)
         {
-            // Handle rotation
             float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
             float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
@@ -208,7 +179,6 @@ public class SCP0173 : SCP
             playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
             transform.Rotate(Vector3.up * mouseX);
 
-            // Handle movement
             float moveHorizontal = Input.GetAxis("Horizontal");
             float moveVertical = Input.GetAxis("Vertical");
 
@@ -244,7 +214,6 @@ public class SCP0173 : SCP
 
     public void FreeFromObserver()
     {
-        // Teleport to the camera's position and orient towards the camera's forward direction once
         var rotation = playerCamera.transform.rotation;
 
         Vector3 spiritForward = playerCamera.transform.forward;
@@ -252,10 +221,10 @@ public class SCP0173 : SCP
         transform.rotation = Quaternion.LookRotation(spiritForward);
         transform.position = playerCamera.transform.position;
 
-        playerCamera.transform.rotation = originalRotation; // Reset the camera rotation to the original
-        playerCamera.transform.position = originalPosition; // Reset the camera position to the original
+        playerCamera.transform.rotation = originalRotation;
+        playerCamera.transform.position = originalPosition;
 
-        isBeingObserved = false; // Now the player is not being observed anymore
+        isBeingObserved = false;
     }
     #endregion
 }
